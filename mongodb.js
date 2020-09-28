fc336936ae9d651e29704242e83934925ca65428
Sampreeth Amith Kumar <sampreethamith65@gmail.com>
2020-09-28 23:07:37 +1000

    latest

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,8 @@
 // Embedding openinghours in placesProfiles
-db.placesProfiles.aggregate([ { $lookup: { from:'openingHours', localField:'_id', foreignField: 'placeID', as:'openingHours'} },{$merge:{into:'placesProfiles'}} ]);
+db.placeProfiles.aggregate([ { $lookup: { from:'openingHours', localField:'_id', foreignField: 'placeID', as:'openingHours'} },{$merge:{into:'placeProfiles'}} ]);
 
 //Insert value Task C2
-db.placesProfiles.insertOne(
+db.placeProfiles.insertOne(
     {
         "_id": "70000",
         "acceptedPaymentModes": "any",
@@ -48,3 +48,60 @@ db.placesProfiles.insertOne(
         ]
       }
 )
+
+// C2 Task 2 - Updating data
+//souce
+// https://stackoverflow.com/questions/12589792/how-to-replace-substring-in-mongodb-document
+db.userProfiles.deleteOne({"_id":1108});
+db.userProfiles.insertOne({
+  "_id": 1108,
+  "favCuisines": "Cafe-Coffee_Shop, Sushi, Latin_American, Deli-Sandwiches, Mexican, Hot_Dogs, American, Fast_Food, Burgers, Asian, Pizzeria, Chinese, Dessert-Ice_Cream, Cafeteria, Japanese, Game, Family, Seafood",
+  "favPaymentMethod": "VISA, cash, MasterCard-Eurocard",
+  "location": {
+    "latitude": "22.143524",
+    "longitude": "-100.98756"
+  },
+  "otherDemographics": {
+    "employment": "student",
+    "religion": "Catholic"
+  },
+  "personalTraits": {
+    "birthYear": "1983",
+    "height": "1.81",
+    "maritalStatus": "single",
+    "weight": "76"
+  },
+  "personality": {
+    "drinkLevel": "abstemious",
+    "favColor": "blue",
+    "interest": "technology",
+    "typeOfWorker": "thrifty-protector"
+  },
+  "preferences": {
+    "ambience": "solitary",
+    "budget": "medium",
+    "dressPreference": "informal",
+    "smoker": "FALSE",
+    "transport": "public"
+  }
+});
+
+db.userProfiles.updateMany(
+  {"_id":1108 },
+  [{
+    $set: {"favCuisines" : {
+      $replaceOne: { input: "$favCuisines", find:"Fast_Food,",replacement:""}
+    }}
+  },{
+    $set : {"favPaymentMethod" : {
+      $replaceOne: {input:"$favPaymentMethod",find:"cash",replacement: "debit_cards"}
+    }}
+  }]
+);
+
+// C2 Task 3 - Delete data
+db.userProfiles.deleteOne({"_id":1063});
+
+
+
+// C3  Task
